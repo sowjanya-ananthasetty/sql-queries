3a9add7335aeeea9cfd8a01af81c7d18a2de7478
Sowjanya Ananthasetty <sowjanyaananthasetty07@gmail.com>
2026-07-20 23:01:53 +0530

    Create Student table and insert sample data
    Created a Student table with sample data for various students.

---
+++ Student_table.sql
@@ -0,0 +1,21 @@
+# Student Table
+CREATE TABLE Student (
+    student_id INT,
+    name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10),
+    course VARCHAR(50),
+    city VARCHAR(50)
+);
+# Insert Data
+INSERT INTO Student VALUES
+(101, 'Ravi', 20, 'Male', 'Python', 'Chennai'),
+(102, 'Anu', 21, 'Female', 'Java', 'Hyderabad'),
+(103, 'Kiran', 22, 'Male', 'SQL', 'Bangalore'),
+(104, 'Divya', 20, 'Female', 'Testing', 'Pune'),
+(105, 'Arun', 23, 'Male', 'Django', 'Mumbai'),
+(106, 'Sneha', 21, 'Female', 'Python', 'Delhi'),
+(107, 'Vijay', 24, 'Male', 'Java', 'Chennai'),
+(108, 'Keerthi', 22, 'Female', 'SQL', 'Kolkata'),
+(109, 'Manoj', 20, 'Male', 'Testing', 'Hyderabad'),
+(110, 'Priya', 23, 'Female', 'Django', 'Bangalore');
